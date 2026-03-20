a44608958b95fceeb18417ce5ccd6c125e7c18dc
¨Jane <smoreira11@hotmail.com>
2026-03-20 12:29:20 +0900

    add

--- dna.py
+++ dna.py
@@ -15,7 +15,6 @@ def main():
         for row in reader:
             database.append(row)
     
-    # Obtener los STRs del archivo (las columnas excepto 'name')
     str_sequences = list(reader.fieldnames[1:]) if database else []
     
     # TODO: Read DNA sequence file into a variable
@@ -27,7 +26,7 @@ def main():
     for str_seq in str_sequences:
         str_counts[str_seq] = longest_match(dna_sequence, str_seq)
     
-    # TODO: Check database for matching profiles
+    # TODO: 
     for person in database:
         match = True
         for str_seq in str_sequences:
@@ -45,38 +44,33 @@ def main():
 def longest_match(sequence, subsequence):
     """Returns length of longest run of subsequence in sequence."""
 
-    # Initialize variables
     longest_run = 0
     subsequence_length = len(subsequence)
     sequence_length = len(sequence) 
 
-    # Check each character in sequence for most consecutive runs of subsequence
+   
     for i in range(sequence_length):
 
-        # Initialize count of consecutive runs
+        
         count = 0
-
-        # Check for a subsequence match in a "substring" (a subset of characters) within sequence
-        # If a match, move substring to next potential match in sequence
-        # Continue moving substring and checking for matches until out of consecutive matches
         while True:
 
-            # Adjust substring start and end
+           
             start = i + count * subsequence_length
             end = start + subsequence_length
 
-            # If there is a match in the substring
+          
             if sequence[start:end] == subsequence:
                 count += 1
             
-            # If there is no match in the substring
+           
             else:
                 break
         
-        # Update most consecutive matches found
+        
         longest_run = max(longest_run, count)
 
-    # After checking for runs at each character in seqence, return longest run found
+  
     return longest_run
 
 
